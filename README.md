ef6698074c7ca38d8bad7963d9643d649a178907
kashiwayaaaa <y_kashiwaya@machiiro.jp>
2018-02-22 16:54:20 +0900

    Add README

---
+++ README.md
@@ -0,0 +1,29 @@
+# Abount
+
+This is tool for ShiftJIS encoding scss file with gulp with windows environment.
+
+# How To Use
+
+Default source directory is './src' and default destination directory is './dist'.
+
+```
+$ . build.sh
+```
+
+If you want to set source directory.
+
+```
+$. build.sh -s src
+```
+
+If you want to set destination directory.
+
+```
+$. build.sh -d dest
+```
+
+# Contributing
+
+Thank you for considering contributing to the gulp-for-shift-jis!
+This is very less influential repository.
+Please do not hesitate to send a pull request.
--- build.sh
+++ build.sh
@@ -1,5 +1,53 @@
-#!/bin/sh
- 
-./bin/nkf32.exe -w src/sample.scss > src/sample_utf.scss
-gulp
+#!/bin/sh -eu
 
+# Default directory setting. It is overwrite by argument.
+SRC_DIR="./src"
+DIST_DIR="./dist"
+
+function usage() {
+cat <<_EOT_
+Usage:
+  $0 [-s dirname] [-d dirname]
+
+Description:
+  hogehogehoge
+
+Options:
+  -s    source dir name
+  -d    dist dir name
+_EOT_
+exit 1
+}
+
+function usage_exit() {
+  echo "Usage: $0 [-s dirname] [-d dirname]" 1>&2
+  exit 1
+}
+
+while getopts s:t: OPT
+do
+    case $OPT in
+        s)  SRC_DIR=$OPTARG;
+            ;;
+        t)  DIST_DIR=$OPTARG;
+            ;;
+        \?) usage_exit
+            ;;
+    esac
+done
+
+# shift $((OPTIND - 1))
+
+echo "source_dir=${SRC_DIR}"
+echo "dist_dir=${DIST_DIR}"
+
+if [ "$COMSPEC" = "" ]; then
+  echo "[Error] Sorry. This tool is only windows environment."
+  exit 1
+fi
+
+for file in `\find ${SRC_DIR} -type f | awk -F/ '{print $NF}'`; do
+  ./bin/nkf32.exe -w ${SRC_DIR}/${file} > ${SRC_DIR}/utf8/${file}
+done
+
+#gulp
